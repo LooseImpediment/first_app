135bb0cb3551a1d082082ab6550cb876a452cbdb
njs <njs@njs-Inspiron-560.(none)>
2011-08-14 09:04:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5726ac9def8acd564af1567056c8577ef7dca2768dd1e3a3c2247cd47e65b9fc570bdd7f1f7326af373847fd52fb055bafc7aa0dd3e90fe504cc42dee6e075a2'
